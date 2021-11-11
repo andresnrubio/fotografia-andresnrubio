8eb640d80b3012b40ee2b7ea1a1b1021f946ebe6
Andres Nicolas Rubio <rubioandres1@gmail.com>
2021-11-11 13:27:29 -0300

    Item count modificado

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,12 +1,22 @@
 import "./ItemDetail.css";
 import { ItemCount } from "../ItemCount/ItemCount";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const ItemDetail = (props) => {
-  console.log(props.item);
+  const [cartButton, setCartButton] = useState(false);
 
+  const [qItemToAdd, setqItemToadd] = useState(0);
+  console.log(qItemToAdd);
   const itemDetail = props.item;
 
-  console.log(itemDetail);
+  const onAdd = (qItemToAdd) => {
+    setqItemToadd(qItemToAdd);
+
+    if (qItemToAdd >= 1) {
+      setCartButton(true);
+    }
+  };
 
   return (
     <div className="ItemDetail">
@@ -25,9 +35,20 @@ export const ItemDetail = (props) => {
       <div className="LateralDetalles">
         <h2>{itemDetail.title}</h2>
         <p>{itemDetail.description}</p>
-        <div className="ItemAdd">
-          <ItemCount initial={itemDetail.initial} stock={itemDetail.stock} />
-        </div>
+
+        {cartButton ? (
+          <Link to="/cart">
+            <button>Ir a carrito</button>
+          </Link>
+        ) : (
+          <div className="ItemAdd">
+            <ItemCount
+              initial={itemDetail.initial}
+              stock={itemDetail.stock}
+              onAdd={onAdd}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
